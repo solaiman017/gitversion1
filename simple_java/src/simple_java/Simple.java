c360a5999423614c6a8f7c6719e0a20b560e88e1
solaiman11373 <65508192+solaiman11373@users.noreply.github.com>
2021-12-09 14:50:24 -0500

    First commite...

---
+++ simple_java/src/simple_java/Simple.java
@@ -0,0 +1,22 @@
+package simple_java;
+
+public class Simple {
+	
+	public void m1()
+	{
+		System.out.println("hello --world");
+		
+	}
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<10;i++){
+			System.out.println("solaiman");
+		}
+	
+		
+		
+		
+	}
+
+}
